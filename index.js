30d4ce3bf088c09a124a6e97f35bdaa06a2e2455
Adrienne <adriennepaquin@gmail.com>
2021-05-30 14:39:03 -0400

    commit tree 3b73ab2ea77726c7686cf7bb5981566f25745b1d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function createEmployeeRecord(array){
+    let testEmployee = {
+        "firstName": array[0],
+        "familyName": array[1],
+        "title": array[2],
+        "payPerHour": array[3],
+        "timeInEvents": [],
+        "timeOutEvents": []
+    }
+    console.log(testEmployee)
+}
